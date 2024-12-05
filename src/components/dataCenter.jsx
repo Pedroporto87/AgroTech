00ebf2fb59dc3822e21e5a47e84270cea4cddce6
Pedroporto87 <pedroporto_8@hotmail.com>
2024-12-05 13:08:28 -0300

    telemetria pt 1

--- src/components/dataCenter.jsx
+++ src/components/dataCenter.jsx
@@ -1,42 +1,55 @@
-import '../style/datacenter.scss';
-import Mapa from '../img/Imagem 01 - Datacenter.png'
-import Edge  from '../img/edge-computing.png' 
+import '../style/datacenter.scss'; 
+import Title from './title';
 
 export const DataCenter = () => {
   return (
-    <section id="datacenter" className='datacenter'>
-      <section className='image-section'>
-        <img src={Mapa} alt="mapa do Brasil"/>
-        <img src={Edge} alt="edge computing" />
+    <>
+    <Title title={"Produtos e Serviços"} description={"Telemetria e Controle"} id={"telemetria-controle"} />
+    <section className='telemetria'>
+      <section className='telemetria-text'>
+        <p>
+        A tecnologia de telemetria se destaca como um dos pilares fundamentais no avanço do agronegócio e da agricultura de precisão. No Agrotech, nossa solução
+        completa via satélite revoluciona a gestão de máquinas e equipamentos, oferecendo uma visibilidade sem precedentes e controle em tempo real para
+        agricultores e gestores.
+        </p>
+        <p>
+        Ao integrar tecnologia de ponta, o Agrotech desenvolve aplicações, sensores e soluções de conectividade totalmente personalizadas, adaptadas às
+        necessidades específicas de cada etapa da cadeia produtiva do agronegócio.
+        </p>
+        <p>
+        Essa customização garante que cada equipamento, desde máquinas agrícolas até pivôs de irrigação, opere com máxima eficiência e produtividade.
+        Segurança e conformidade, assegurando que todas as operações estejam alinhadas com os regulamentos vigentes.
+        </p>
       </section>
-      <section className='text-section'>
-        <section className='header'>
-          <h1>Data Center</h1>
-          <p>armazenamento e processamento</p>
-        </section>
-        <section className='content'>
-          <p>Solução completa de construção e certificação de ambientes
-            de Datacenter e Mini Datacenter(SmartPoP) para o mercado do 
-            agronegócio
-          </p>
-          <p>O ecossistema de edge datacenter do AgroTech | Trusted, contra 
-            com o desenvolvimento de projetos, instalação e fornecimento
-            de equipamentos no modelo DaaS(Datacenter as a Service) com 
-            100% do valor investido como Opex para o cliente, 
-            cooperativa, agroindústria, provedores e operadoras.
-          </p>
-          <section className='data-ul'>
-            <p>Ambientes preparados para:</p>
-            <ul>
-              <li>Colocation</li>
-              <li>Interconnect</li>
-              <li>Cross Connect</li>
-              <li>Metro & Intermetro Connect</li>
-              <li>Last Mile Connect</li>
-            </ul>
-          </section>
-        </section>
+      <section className='telemetria-ol'>
+        <h1><strong>Importância da Telemetria no Agronegócio:</strong></h1>
+        <ol>
+          <li>
+          <strong>Eficiência Operacional:</strong> A telemetria permite o monitoramento contínuo e detalhado de máquinas e
+          implementos agrícolas, como tratores e colheitadeiras. Isso resulta em uma operação mais eficiente, com
+          menor tempo de inatividade e manutenção preditiva, evitando quebras inesperadas.
+          </li>
+          <li>
+          <strong>Agricultura de Precisão:</strong> Sensores avançados possibilitam a coleta de dados em tempo real sobre as
+          condições do solo, clima e desempenho das colheitas. Com isso, os agricultores podem tomar decisões mais
+          informadas e precisas, otimizando o uso de insumos e recursos naturais
+          </li>
+          <li>
+          <strong>Gerenciamento Remoto:</strong> As soluções de telemetria via satélite permitem o controle remoto de
+          equipamentos como bombas d’água e pivôs de irrigação, assegurando que as operações críticas continuem
+          sem interrupção, mesmo em locais remotos.
+          </li>
+          <li>
+          <strong>Sustentabilidade:</strong> Ao melhorar a eficiência e reduzir o consumo de recursos, a telemetria contribui para
+          práticas agrícolas mais sustentáveis, minimizando o impacto ambiental.
+          </li>
+          <li><strong>Segurança e Conformidade:</strong>
+           A telemetria também auxilia no acompanhamento de normas de segurança e
+          conformidade, assegurando que todas as operações estejam alinhadas com os regulamentos vigentes
+          </li>
+        </ol>
       </section>
     </section>
+    </>
   );
 }
